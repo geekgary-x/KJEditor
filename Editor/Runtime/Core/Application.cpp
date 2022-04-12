1766f32c7a6231789de8fb7b9d3d6f689f5865c3
xhguleixin123 <18017158729@163.com>
2022-04-12 14:45:51 +0800

    🎉 init: 初始化构建项目

---
+++ Editor/Runtime/Core/Application.cpp
@@ -0,0 +1,13 @@
+#include "Core/Application.h"
+
+namespace Soarscape
+{
+    Application::Application()
+    {
+        
+    }
+    Application::~Application()
+    {
+
+    }
+}
---
+++ Editor/Runtime/Core/Application.h
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace Soarscape {
+    class Application
+    {
+    public:
+        Application();
+        ~Application();
+    };
+}
