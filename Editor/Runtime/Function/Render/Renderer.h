bad7405f91ed0633f85ce6728f4e95e0e96d8c83
xhguleixin123 <18017158729@163.com>
2022-04-13 15:06:48 +0800

    🐞 fix: 解决osgqt头文件问题

--- Editor/Runtime/Function/Render/Renderer.h
+++ Editor/Runtime/Function/Render/Renderer.h
@@ -1,13 +1,14 @@
 #pragma once
 #include "Core/Base/PublicSingleton.h"
-#include <osgQOpenGL/OSGRenderer>
+#include <osgQOpenGL/osgQOpenGLWidget>
+
 namespace Soarscape
 {
 	class Renderer : public PublicSingleton<Renderer>
 	{
 	public:
-
+		Renderer() = delete;
 	private:
-
+		osgQOpenGLWidget m_osgQt;
 	};
 }
