655cbc70098379e096b5f9383705e58c078c916d
xhguleixin123 <18017158729@163.com>
2022-04-19 15:50:16 +0800

    🎈 perf: 运行时和qt隔离

--- Editor/Runtime/Function/Scene/Components.h
+++ Editor/Runtime/Function/Scene/Components.h
@@ -1,30 +1,7 @@
 #pragma once
-#include <osg/Vec3>
-#include <osg/Matrix>
 #include <string>
 namespace Soarscape
 {
-	struct TransformComponent
-	{
-		osg::Vec3 Translation = { 0.0f, 0.0f, 0.0f };
-		osg::Vec3 Rotation = { 0.0f, 0.0f, 0.0f };
-		osg::Vec3 Scale = { 1.0f, 1.0f, 1.0f };
-
-		TransformComponent() = default;
-		TransformComponent(const TransformComponent&) = default;
-		TransformComponent(const osg::Vec3& translation)
-			: Translation(translation) {}
-
-		osg::Matrix GetTransform() const
-		{
-			osg::Quat xQuat(Rotation.x(), osg::Vec3(1, 0, 0));
-			osg::Quat yQuat(Rotation.y(), osg::Vec3(0, 1, 0));
-			osg::Quat zQuat(Rotation.z(), osg::Vec3(0, 0, 1));
-			return osg::Matrix::translate(Translation) * osg::Matrix::rotate(xQuat) \
-				* osg::Matrix::rotate(yQuat) * osg::Matrix::rotate(zQuat) * osg::Matrix::scale(Scale);
-		}
-	};
-
 	struct TagComponent
 	{
 		std::string Tag;
--- Editor/Runtime/Function/Scene/Scene.cpp
+++ Editor/Runtime/Function/Scene/Scene.cpp
@@ -5,7 +5,6 @@ namespace Soarscape
 	SceneObject Scene::CreateObject(const std::string& name)
 	{
 		SceneObject entity = { m_Registry.create(), this};
-		entity.AddComponent<TransformComponent>();
 		auto& Tag = entity.AddComponent<TagComponent>();
 		Tag.Tag = name.empty() ? "Entity" : name;
 		return entity;
