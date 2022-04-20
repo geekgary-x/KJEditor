e751acd7b004d4682fcb9b884b7d12d42adc8f68
xhguleixin123 <18017158729@163.com>
2022-04-20 15:18:53 +0800

    ✨ feat: 测试使用生成的SPIR_V

--- Editor/App/source/EditorRenderWidget.cpp
+++ Editor/App/source/EditorRenderWidget.cpp
@@ -4,36 +4,11 @@
 #include <Function/Render/FrameBuffer.h>
 #include <EditorUI.h>
 #include <iostream>
+#include "mesh_vert.h"
+#include "mesh_frag.h"
 namespace Soarscape
 {
-    const uint32_t testvert[] = {
-    0x07230203,0x00010000,0x0008000a,0x0000000e,0x00000000,0x00020011,0x00000001,0x0006000b,
-    0x00000001,0x4c534c47,0x6474732e,0x3035342e,0x00000000,0x0003000e,0x00000000,0x00000001,
-    0x0006000f,0x00000004,0x00000004,0x6e69616d,0x00000000,0x00000009,0x00030010,0x00000004,
-    0x00000007,0x00030003,0x00000002,0x0000014a,0x00040005,0x00000004,0x6e69616d,0x00000000,
-    0x00050005,0x00000009,0x67617246,0x6f6c6f43,0x00000072,0x00040047,0x00000009,0x0000001e,
-    0x00000000,0x00020013,0x00000002,0x00030021,0x00000003,0x00000002,0x00030016,0x00000006,
-    0x00000020,0x00040017,0x00000007,0x00000006,0x00000004,0x00040020,0x00000008,0x00000003,
-    0x00000007,0x0004003b,0x00000008,0x00000009,0x00000003,0x0004002b,0x00000006,0x0000000a,
-    0x3f800000,0x0004002b,0x00000006,0x0000000b,0x3dcccccd,0x0004002b,0x00000006,0x0000000c,
-    0x00000000,0x0007002c,0x00000007,0x0000000d,0x0000000a,0x0000000b,0x0000000c,0x0000000c,
-    0x00050036,0x00000002,0x00000004,0x00000000,0x00000003,0x000200f8,0x00000005,0x0003003e,
-    0x00000009,0x0000000d,0x000100fd,0x00010038
-    };
-    const uint32_t testfrag[] = {
-    0x07230203,0x00010000,0x0008000a,0x0000000e,0x00000000,0x00020011,0x00000001,0x0006000b,
-    0x00000001,0x4c534c47,0x6474732e,0x3035342e,0x00000000,0x0003000e,0x00000000,0x00000001,
-    0x0006000f,0x00000004,0x00000004,0x6e69616d,0x00000000,0x00000009,0x00030010,0x00000004,
-    0x00000007,0x00030003,0x00000002,0x0000014a,0x00040005,0x00000004,0x6e69616d,0x00000000,
-    0x00050005,0x00000009,0x67617246,0x6f6c6f43,0x00000072,0x00040047,0x00000009,0x0000001e,
-    0x00000000,0x00020013,0x00000002,0x00030021,0x00000003,0x00000002,0x00030016,0x00000006,
-    0x00000020,0x00040017,0x00000007,0x00000006,0x00000004,0x00040020,0x00000008,0x00000003,
-    0x00000007,0x0004003b,0x00000008,0x00000009,0x00000003,0x0004002b,0x00000006,0x0000000a,
-    0x3f800000,0x0004002b,0x00000006,0x0000000b,0x3dcccccd,0x0004002b,0x00000006,0x0000000c,
-    0x00000000,0x0007002c,0x00000007,0x0000000d,0x0000000a,0x0000000b,0x0000000c,0x0000000c,
-    0x00050036,0x00000002,0x00000004,0x00000000,0x00000003,0x000200f8,0x00000005,0x0003003e,
-    0x00000009,0x0000000d,0x000100fd,0x00010038
-    };
+    
     const uint32_t aaavert = 0;
 	EditorRendererWidget::EditorRendererWidget(QWidget* parent)
 		: QOpenGLWidget(parent)
@@ -78,72 +53,21 @@ namespace Soarscape
         // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
         glBindVertexArray(0);
         m_QuadVAO = VAO;
-        // 顶点着色器
-        const char* vertexShaderSource = "#version 330 core\n"
-            "layout (location = 0) in vec3 aPos;\n"
-            "void main()\n"
-            "{\n"
-            "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-            "}\0";
-        unsigned int vertexShader;
-        vertexShader = glCreateShader(GL_VERTEX_SHADER);
-        glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-        glCompileShader(vertexShader);
-        int success;
-        char infoLog[512];
-        glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-        if (!success)
-        {
-            glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-            std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"
-                << infoLog << std::endl;
-        }
-        // 片元着色器
-        const char* fragmentShaderSource = "#version 330 core\n"
-            "out vec4 FragColor;\n"
-            "void main()\n"
-            "{\n"
-            "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-            "}\0";
-        unsigned int fragmentShader;
-        fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-        glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-        glCompileShader(fragmentShader);
-        glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-        if (!success)
-        {
-            glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-            std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n"
-                << infoLog << std::endl;
-        }
-
-        // 着色程序
-        unsigned int shaderProgram;
-        shaderProgram = glCreateProgram();
-        glAttachShader(shaderProgram, vertexShader);
-        glAttachShader(shaderProgram, fragmentShader);
-        glLinkProgram(shaderProgram);
-        glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-        if (!success)
-        {
-            glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-            std::cout << "ERROR::SHADER::PROGROG::COMPILATION_FAILED\n"
-                << infoLog << std::endl;
-        }
-        // glUseProgram(shaderProgram);
 
         GLuint program = glCreateProgram();
         GLuint testfragid = glCreateShader(GL_FRAGMENT_SHADER);
-        glShaderBinary(1, &testfragid, GL_SHADER_BINARY_FORMAT_SPIR_V, testfrag, sizeof(testfrag));
+        glShaderBinary(1, &testfragid, GL_SHADER_BINARY_FORMAT_SPIR_V, mesh_frag, sizeof(mesh_frag));
         glSpecializeShader(testfragid, "main", 0, nullptr, nullptr);
         glAttachShader(program, testfragid);
 
         GLuint testvertid = glCreateShader(GL_VERTEX_SHADER);
-        glShaderBinary(1, &testvertid, GL_SHADER_BINARY_FORMAT_SPIR_V, testvert, sizeof(testvert));
+        glShaderBinary(1, &testvertid, GL_SHADER_BINARY_FORMAT_SPIR_V, mesh_vert, sizeof(mesh_vert));
         glSpecializeShader(testvertid, "main", 0, nullptr, nullptr);
         glAttachShader(program, testvertid);
         glLinkProgram(program);
-        glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+        int success = 0;
+        char infoLog[512] = {0};
+        glGetProgramiv(program, GL_LINK_STATUS, &success);
         if (!success)
         {
             glGetProgramInfoLog(program, 512, NULL, infoLog);
@@ -152,8 +76,6 @@ namespace Soarscape
         }
         glUseProgram(program);
 
-        glDeleteShader(vertexShader);
-        glDeleteShader(fragmentShader);
 	}
 
 	void EditorRendererWidget::resizeGL(int w, int h)
